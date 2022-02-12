239e8a0c83b5e30153b03153797611a4376a9b76
Paolo Rosi <bradiporitmico@gmail.com>
2022-02-12 14:34:38 +0100

    refurbished filter api path calls

--- src/Newsboy/Client/v1/Filter.php
+++ src/Newsboy/Client/v1/Filter.php
@@ -10,39 +10,44 @@ class Filter{
 	}
 
 	public function generateTable(string $filter_name){
-		$res = $this->api->call ("/post/filter/generate/{$filter_name}");
+		$res = $this->api->call ("filter/generate/{$filter_name}");
 		return $res->response;
 	}
 
 	public function duplicate(string $source_id){
-		$res = $this->api->call ("post/filter/duplicate/{$source_id}");
+		$res = $this->api->call ("filter/duplicate/{$source_id}");
 	}
 
 	public function fullList(){
-		$res = $this->api->call ("post/filter/fulllist");
+		$res = $this->api->call ("filter/fulllist");
 		return $res->response;
 	}
 
 	public function list(string $parent = null){
 		if ($parent)
-			$res = $this->api->call ("post/filter/list/{$parent}");
+			$res = $this->api->call ("filter/list/{$parent}");
 		else
-			$res = $this->api->call ("post/filter/list");
+			$res = $this->api->call ("filter/list");
 		return $res->response;
 	}
 
 	public function tree(string $parent = null){
-		$res = $this->api->call ("post/filter/tree/{$parent}");
+		$res = $this->api->call ("filter/tree/{$parent}");
+		return $res->response;
+	}
+
+	public function delete(string $id){
+		$res = $this->api->call ("filter/delete/{$id}", null, 'DELETE');
 		return $res->response;
 	}
 
 	public function info(string $filter_name){
-		$res = $this->api->call ("post/filter/info/{$filter_name}");
+		$res = $this->api->call ("filter/info/{$filter_name}");
 		return $res->response;
 	}
 
 	public function statsMunicipality(string $filter_name){
-		$res = $this->api->call ("post/filter/{$filter_name}/stats/municipality");
+		$res = $this->api->call ("filter/{$filter_name}/stats/municipality");
 		return $res->response;
 	}
 
