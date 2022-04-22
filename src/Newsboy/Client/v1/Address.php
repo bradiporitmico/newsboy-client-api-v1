00a739245d1f56e77364f73b27cefbed43e30b72
Paolo Rosi <bradiporitmico@gmail.com>
2022-04-22 23:08:23 +0200

    Nuova classe per la gestione degli indirizzi

---
+++ src/Newsboy/Client/v1/Address.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Newsboy\Client\v1;
+
+class Address{
+
+	private $api = null;
+
+	public function __construct (Api $api){
+		$this->api = $api;
+	}
+
+	public function municipalitySearch(string $term){
+		$res = $this->api->call ("address/search/municipality/{$term}");
+		return $res->response;
+	}
+
+
+
+}
+
