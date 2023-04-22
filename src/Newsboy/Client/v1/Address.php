0ea917b6dc11d15eef77be8d3b6e5882628a5c29
Paolo Rosi <bradiporitmico@gmail.com>
2023-04-22 13:09:05 +0200

    Aggiunti metodi per recuperare le informazioni sul comune e sulla provincia

--- src/Newsboy/Client/v1/Address.php
+++ src/Newsboy/Client/v1/Address.php
@@ -10,11 +10,39 @@ class Address{
 		$this->api = $api;
 	}
 
+	/**
+	 * Cerca fra i comuni partendo dal nome
+	 *
+	 * @param string $term Il test da cercare fra i nomi di comuni
+	 */
 	public function municipalitySearch(string $term){
 		$res = $this->api->call ("address/search/municipality/".urlencode($term));
 		return $res->response;
 	}
 
+
+	/**
+	 * Torna le info di un comune (o di tutti i comuni)
+	 *
+	 * @param string $id L'id (slug) del comune. Se non specificato verrà tornata la lista completa di tutti i comuni
+	 */
+	public function municipality(string $id = ''){
+		$id = $id ? "/{$id}" : '';
+		$res = $this->api->call ("address/municipality{$id}");
+		return $res->response;
+	}
+
+
+	/**
+	 * Torna le info di una provincia
+	 *
+	 * @param string $id L'id (slug) della provincia. Se non specificato verrà tornata la lista completa di tutte le province
+	 */
+	public function province(string $id = ''){
+		$id = $id ? "/{$id}" : '';
+		$res = $this->api->call ("address/province{$id}");
+		return $res->response;
+	}
 
 
 }
